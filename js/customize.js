a2ad81e50170cd58e7d7c528a27803635721a01e
MrJohnson <59217159+johnsonou1025@users.noreply.github.com>
2024-11-04 18:03:45 +0800

    Joinjo
    add contact form

--- js/customize.js
+++ js/customize.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     })
     // -----------
     // $('.nav-menu').on('click', 'a', function (event) {
-    $(".nav-menu a").not(':last').on('click', function (event) {
+    $(".nav-menu a").on('click', function (event) {
         $("body").removeClass("lock");
         $(".nav-menu").removeClass("open");
         var _headerHeight = $("header").height();
@@ -31,6 +31,56 @@ $(document).ready(function () {
     });
     $(".card-slider i:last-child").click(function () {
         $(".card-group li:first-child").insertAfter($(".card-group li:last-child"));
+    });
+
+    // contact
+    $('#sendEmail').click(function () {
+        // 取得每個欄位的值
+        var name = $('#name').val();
+        var email = $('#email').val();
+        var message = $('#message').val();
+        var errorMessage;
+        var isValid = true; // 表示驗證通過
+
+        $(".form-text").removeClass("error")
+
+        // 檢查email
+        var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!emailPattern.test(email)) {
+            isValid = false;
+            $(this).parent().find("#email").parent().addClass("error");
+            errorMessage = "please check for the correct email format."
+        }
+
+        // 檢查textarea不是空白
+        $('textarea').each(function () {
+            if ($(this).val().trim() === "") {
+                isValid = false;
+                $(this).parent().addClass("error");
+                errorMessage = "Please fill in this field."
+            }
+        });
+
+        // 檢查input不是空白
+        $('input').each(function () {
+            if ($(this).val().trim() === "") {
+                isValid = false;
+                $(this).parent().addClass("error");
+                errorMessage = "Please fill in this field."
+            }
+        });
+
+        if (!isValid) {
+            $('#error').show().text(errorMessage);
+        } else {
+            // 將欄位值插入到 mailto 字串
+            var mailtoLink = "mailto:" + "johnsonou1025@gmail.com" +
+                "?subject=" + encodeURIComponent(name) +
+                "&body=" + encodeURIComponent("姓名: " + name + "\n\n信箱:" + email + "\n\n訊息:\n" + message);
+            // 開啟郵件客戶端
+            window.location.href = mailtoLink;
+        }
+
     });
 
 });
