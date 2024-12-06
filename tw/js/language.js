ce0b68a4b32fe8dbc804b7e843412f4db08f97bb
MrJohnson <59217159+johnsonou1025@users.noreply.github.com>
2024-12-06 19:38:08 +0800

    Update language.js

--- tw/js/language.js
+++ tw/js/language.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
     // change language
     $(".lang").click(function () {
         let currentUrl = window.location.href;
-        // let newUrl = currentUrl.replace("joinjo.studio/tw/", "joinjo.studio/");
-        let newUrl = currentUrl.replace("5502/tw/", "5502/");
+        let newUrl = currentUrl.replace("joinjo.studio/tw/", "joinjo.studio/");
+        // let newUrl = currentUrl.replace("5502/tw/", "5502/");
         window.location.href = newUrl;
         console.log(newUrl)
     })
