6e3cf1f1b9be52a0a38c79581de15734438785e5
MrJohnson <59217159+johnsonou1025@users.noreply.github.com>
2025-03-25 23:31:32 +0800

    casino-test adjust

--- client/casino-test/js/customer.js
+++ client/casino-test/js/customer.js
@@ -4,43 +4,38 @@ $(function () {
     // freezed
     $(window).on('scroll', function () {
         var currentScrollTop = $(this).scrollTop();
-        console.log(currentScrollTop);
+        // console.log(currentScrollTop);
+        var topHeight = ($(".freeze-box").height()) * 2 + 26;
         if (!$("body").hasClass("freezed")) {
             if (currentScrollTop >= 176) {
                 $("body").addClass("freezed");
-                // $('html, body').scrollTop(1);
-                // $('html, body').animate({ scrollTop: 1 }, 'fast');
+                $(".freezed main").css('padding-top', topHeight);
+
             }
         } else {
             if (currentScrollTop < 176) {
                 $("body").removeClass("freezed");
-                // $('html, body').scrollTop(175);
-                // $('html, body').animate({ scrollTop: 175 }, 'fast');
+                $("main").css('padding-top', 0);
             }
         }
     })
 
     // jackpot number 
-
     let num = 0;
     let numEnd = 8888888;
     let interval = setInterval(function () {
         if (num >= numEnd) {
-            let strNum = Math.floor(numEnd).toString().padStart(9, "0"); // 用 0 補滿 4 位數
-            $(".number span:nth-child(1)").text(strNum.charAt(0));
-            $(".number span:nth-child(3)").text(strNum.charAt(1));
-            $(".number span:nth-child(4)").text(strNum.charAt(2));
-            $(".number span:nth-child(5)").text(strNum.charAt(3));
-            $(".number span:nth-child(7)").text(strNum.charAt(4));
-            $(".number span:nth-child(8)").text(strNum.charAt(5));
-            $(".number span:nth-child(9)").text(strNum.charAt(6));
-            $(".number span:nth-child(11)").text(strNum.charAt(7));
-            $(".number span:nth-child(12)").text(strNum.charAt(8));
-            clearInterval(interval); // 停止動畫
+            numberRun(numEnd);
+            clearInterval(interval);
             return;
         }
+        numberRun(num);
+        num = num + (numEnd / 100);
 
-        let strNum = Math.floor(num).toString().padStart(9, "0"); // 用 0 補滿 4 位數
+    }, 10); // 5ms 增加 1
+    // set number function
+    function numberRun(e) {
+        let strNum = Math.floor(e).toString().padStart(9, "0");
         $(".number span:nth-child(1)").text(strNum.charAt(0));
         $(".number span:nth-child(3)").text(strNum.charAt(1));
         $(".number span:nth-child(4)").text(strNum.charAt(2));
@@ -50,10 +45,7 @@ $(function () {
         $(".number span:nth-child(9)").text(strNum.charAt(6));
         $(".number span:nth-child(11)").text(strNum.charAt(7));
         $(".number span:nth-child(12)").text(strNum.charAt(8));
-
-        num = num + (numEnd / 100);
-
-    }, 10); // 5ms 增加 1
+    }
 
     // click tags 
     $(".tags-box li").click(function () {
