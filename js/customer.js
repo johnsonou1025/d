3ee0d8c9fd909ab44360d5782a84addd0ce2af7f
MrJohnson <59217159+johnsonou1025@users.noreply.github.com>
2025-12-25 11:02:30 +0800

    Update customer.js

--- js/customer.js
+++ js/customer.js
@@ -1,3 +1,12 @@
+$(".next").click(function () {
+    $(this).removeClass("active").siblings().addClass("active");
+    $(".services ul").css("transform", "translateX(-50%)");
+})
+$(".prev").click(function () {
+    $(this).removeClass("active").siblings().addClass("active");
+    $(".services ul").css("transform", "translateX(0%)");
+})
+
 // new fullpage('.page', {
 //     sectionSelector: '.section'
 // });
