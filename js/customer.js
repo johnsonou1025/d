aa8e6a109ccfc0964b0451482719e2113503c2a0
MrJohnson <59217159+johnsonou1025@users.noreply.github.com>
2026-04-17 16:07:45 +0800

    Update customer.js
    Signed-off-by: MrJohnson <59217159+johnsonou1025@users.noreply.github.com>

--- js/customer.js
+++ js/customer.js
@@ -11,31 +11,3 @@ $(".prev").click(function () {
 //     sectionSelector: '.section'
 // });
 
-function checkVideoSource() {
-    var video = $('#kv-video');
-    var source = video.find('source');
-    var windowWidth = $(window).width();
-    var currentSrc = source.attr('src');
-    var targetSrc = (windowWidth <= 768) ? 'img/kv-mobile.mp4' : 'img/kv.mp4';
-
-    // 只有當目標來源與當前來源不同時，才執行切換
-    if (currentSrc !== targetSrc) {
-        source.attr('src', targetSrc);
-        video[0].load(); // 必須呼叫 load() 讓瀏覽器重新讀取來源
-        video[0].play(); // 重新播放
-    }
-}
-
-$(document).ready(function() {
-    // 1. 初始化檢查
-    checkVideoSource();
-
-    // 2. 監聽視窗縮放 (加入 debounce 避免過度消耗效能)
-    var resizeTimer;
-    $(window).on('resize', function() {
-        clearTimeout(resizeTimer);
-        resizeTimer = setTimeout(function() {
-            checkVideoSource();
-        }, 250); 
-    });
-});
